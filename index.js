35df52db1698be1fcee35999617d37ab1ac5b68f
Jonatha <johnninajoao@gmail.com>
2023-08-19 18:42:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+io.on("connection", (socket)=>{
+    socket.on("disconnect", ()=>{
+        console.log('X desconectou: ' + socket.id);
+    })
+    socket.on("msg", (data)=>{
+        io.emit("showmsg", data)
+        console.log(data);
+    })
+})
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req,res) =>{
+    res.render("index")
+})
+
+http.listen(3000, ()=>{
+    console.log('App rodando');
+})
